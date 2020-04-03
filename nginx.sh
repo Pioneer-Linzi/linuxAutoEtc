7c2f583fcf7028668ae83058a85ece1b4a68a780
赵林林 <linlin.zhao@ximalaya.com>
2020-04-03 19:52:55 +0800

    nginx

--- nginx.sh
+++ nginx.sh
@@ -1,1 +1,1 @@
-docker run -d --name nginx -p 80:80 -v `pwd`/www:/www -v `pwd`/config:/etc/nginx/  -v `pwd`/logs:/var/log/nginx nginx
+docker run -d --name nginx -p 80:80 -v `pwd`/www:/www -v `pwd`/config:/etc/nginx/sites-enabled   -v `pwd`/logs:/var/log/nginx nginx
