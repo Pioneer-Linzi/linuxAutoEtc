55c7dc7c616af31ece69c9f6558dd4bd4710f996
赵林林 <linlin.zhao@ximalaya.com>
2020-04-03 13:26:37 +0800

    fixbug: (jenkins) --restarts-always

--- jenkins.sh
+++ jenkins.sh
@@ -1,1 +1,1 @@
-docker run -d --name jenkins --restart=alaways  -p 8081:8080 -v /home/jenkins:/home/jenkins jenkins/jenkins:lts 
+docker run -d --name jenkins --restart=always  -p 8081:8080 -v /home/jenkins:/home/jenkins jenkins/jenkins:lts 
